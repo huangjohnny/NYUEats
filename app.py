55b9db73d6e97506222070e28ee9adcee477dff0
Max Kim <you@example.com>
2018-03-24 21:50:26 -0400

    added connection to MySQL database

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'ubuntu'
+app.config['MYSQL_PASSWORD'] = 'cafe'
+app.config['MYSQL_DB'] = 'cafeteria'
+mysql = MySQL(app)
+cur = mysql.connection.cursor()
 
 #homepage
 @app.route('/')
@@ -17,6 +24,13 @@ def selection():
 @app.route('/menu')
 def menu():
 	return render_template('menu.html')
+
+#test connection to MySQL database
+@app.route('/db')
+def connect():
+	cur.execute('''SHOW TABLES''')
+	result = cur.fetchall()
+	return str(result)
 	
 if __name__ == "__main__":
 	app.run(debug=True)
